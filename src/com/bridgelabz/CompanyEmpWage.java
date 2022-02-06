3eac74f3436b8cb46a488c785b4df3cf7e39a9bf
shivkumar <hallaleshivu16@gmail.com>
2022-02-06 13:59:40 +0530

    Modified the Employee Wages Oops Concept

---
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class CompanyEmpWage {
+    public int maxHrsPerMonth
+            ;
+
+    public CompanyEmpWage(String company, int wagePerHr, int numOfWorkingDays, int maxHrsPerMonth) {
+
+    }
+
+    public void setTotalEmpWage(int calculateEmpWage) {
+    }
+}
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,28 +1,35 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+public class EmployeeWageBuilder implements EmployeeWage {
+    private static final int IS_PART_TIME = ;
+    private static final int IS_FULL_TIME = ;
+    private int noOfCompanies = 0;
 
-public class Main {
+    //ArrayList for Company Wage Object
+    ArrayList<CompanyEmpWage> lstCompanyEmpWages;
 
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int WAGE_PER_HOUR = 20;
-    public static final int NUM_WORKING_DAYS = 20;
-    public static final int MAX_HOUR_IN_MONTH = 100;
+    public EmployeeWageBuilder() {
+        lstCompanyEmpWages = new ArrayList<>();
+    }
 
-    public static void main(String[] args) {
+    private void addCompanyEmpWages(String company, int wagePerHr, int numOfWorkingDays, int maxHrsPerMonth) {
+        lstCompanyEmpWages.add(new CompanyEmpWage(company, wagePerHr, numOfWorkingDays, maxHrsPerMonth));
+    }
 
-        //Welcome Message for Initial Purpose
-        System.out.println("Welcome to Employee Wages Problem Developed by Tahir Mansuri.");
-        //Variable for Calculating Employee Wage
-        int empWage = 0;
-        int empHrs = 0;
-        int totalEmpWage = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        while( totalEmpHrs <= MAX_HOUR_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
+    public void calculateEmpWage() {
+        for (CompanyEmpWage companyEmpWage : lstCompanyEmpWages) {
+            companyEmpWage.setTotalEmpWage(this.calculateEmpWage(companyEmpWage));
+            System.out.println(companyEmpWage.toString());
+        }
+    }
 
+    //Method to Calculate Employee Wage by Hours and Type
+    public int calculateEmpWage(CompanyEmpWage companyEmpWage) {
+        // Local Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        //Compute Emp Wage for Month
+        while (totalEmpHrs <= companyEmpWage.maxHrsPerMonth && totalWorkingDays < companyEmpWage.numWorkingDays) {
             totalWorkingDays++;     //Incrementing Working Days
-
-            //Computation
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
                 case IS_PART_TIME:
@@ -38,14 +45,18 @@ public class Main {
                     empHrs = 0;
                     break;
             }
-            totalEmpHrs += empHrs;      //Calculating Total Hours
-            empWage = WAGE_PER_HOUR * empHrs;   // Calculating Employee Wage if Employee is Present as PART TIME Or FULL TIME
-            totalEmpWage = empWage + totalEmpWage;
-            System.out.println("Employee Wage = " + empWage);
-            System.out.println("Days:"+totalWorkingDays+"\tTotal Hours:"+totalEmpHrs);
+            totalEmpHrs += empHrs;
+            System.out.println("Day No :" + totalWorkingDays + " Emp Hrs: " + empHrs);
         }
+        return totalEmpHrs * companyEmpWage.wagePerHr;
+    }
 
-        //Displaying Total Emp Wages
-        System.out.println("Total Employee Wages = "+ totalEmpWage);
+    public static void main(String[] args) {
+        //Welcome Message for Initial Purpose
+        System.out.println("Welcome to Employee Wages Problem Developed by Tahir Mansuri.");
+        EmployeeWageBuilder employeeWageBuilder = new EmployeeWageBuilder();
+        employeeWageBuilder.addCompanyEmpWages("JIO", 20, 5, 25);
+        employeeWageBuilder.addCompanyEmpWages("VODAFONE", 15, 30, 30);
+        employeeWageBuilder.calculateEmpWage();
     }
 }
